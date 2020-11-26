cd055c83f85f3f585a0ee0fd5264077790bc9ad5
Antony Ni <ni96@purdue.edu>
2020-11-26 12:52:40 -0600

    Server and client

--- src/Client.java
+++ src/Client.java
@@ -1,100 +1,56 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
-public class Client implements Runnable {
-    JButton homeButton;
-    JButton saveButton;
-    JButton logoutButton;
-    JButton deleteButton;
-    JTextField nameField;
-    JTextField usernameField;
-    JTextField passwordField;
-    JLabel nameLabel;
-    JLabel usernameLabel;
-    JLabel passwordLabel;
+public class Client {
+    private String host;
+    private int port;
 
-    ActionListener actionListener = new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == homeButton) {
-                homePage();
-            }
-            if (e.getSource() == saveButton) {
-                saveAccount();
-            }
-            if (e.getSource() == logoutButton) {
-                logout();
-            }
-            if (e.getSource() == deleteButton) {
-                deleteAccount();
-            }
+    public static void main(String[] args) throws IOException {
+        Client user = new Client("127.0.0.1", 8080);
+        user.run();
+    }
+
+    public Client(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void run() throws IOException {
+        Socket user = new Socket(host, port);
+        System.out.println("User has connected to the server");
+
+        PrintStream outputStream = new PrintStream(user.getOutputStream());
+
+        // Asks for Name and sends to server
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Name:");
+        String name = scan.nextLine();
+        outputStream.println(name);
+
+        // Asks for Username and sends to server
+        System.out.println("Username:");
+        String username = scan.nextLine();
+        outputStream.println(username);
+
+        // Asks for Password and sends to server
+        System.out.println("Password:");
+        String password = scan.nextLine();
+        outputStream.println(password);
+
+        // Thread for handling messages
+        Thread t1 = new Thread(new MessageHandler(user.getInputStream()));
+        t1.start();
+
+        while (scan.hasNextLine()) {
+            outputStream.println(scan.nextLine());
         }
-    };
-    // Returns to the main page
-    public void homePage() {
 
-    }
-    // Updates the user's information with the new inputted info
-    public void saveAccount() {
-
-    }
-    // Logs out the user. Will return to the login page
-    public void logout() {
-
-    }
-    // Deletes the account and removes them from their conversations
-    public void deleteAccount() {
-
+        outputStream.close();
+        scan.close();
+        user.close();
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Client());
-    }
-
-    public void run() {
-        // testUser is here for testing purposes
-        User testUser = new User("Jimmy", "jimmy123", "jimmypassword");
-
-        JFrame frame = new JFrame("Messaging App");
-        Container content = frame.getContentPane();
-        content.setLayout(new BorderLayout());
-
-        // Contains all the information fields to view and edit.
-        // Includes the user's "Name", "Username", and "Password"
-        JPanel infoPanel = new JPanel();
-        nameLabel = new JLabel("Name");
-        nameField = new JTextField(testUser.getName(), 10);
-        usernameLabel = new JLabel("Username");
-        usernameField = new JTextField(testUser.getUsername(), 10);
-        passwordLabel = new JLabel("Password");
-        passwordField = new JPasswordField(testUser.getPassword(), 10);
-        infoPanel.add(nameLabel);
-        infoPanel.add(nameField);
-        infoPanel.add(usernameLabel);
-        infoPanel.add(usernameField);
-        infoPanel.add(passwordLabel);
-        infoPanel.add(passwordField);
-        content.add(infoPanel, BorderLayout.CENTER);
-
-        // Shows the "Home", "Save", and "Delete" buttons at the bottom of the GUI
-        JPanel buttonsPanel = new JPanel();
-        homeButton = new JButton("Home");
-        homeButton.addActionListener(actionListener);
-        saveButton = new JButton("Save");
-        saveButton.addActionListener(actionListener);
-        logoutButton = new JButton("Logout");
-        logoutButton.addActionListener(actionListener);
-        deleteButton = new JButton("Delete");
-        deleteButton.addActionListener(actionListener);
-        buttonsPanel.add(homeButton);
-        buttonsPanel.add(saveButton);
-        buttonsPanel.add(logoutButton);
-        buttonsPanel.add(deleteButton);
-        content.add(buttonsPanel, BorderLayout.SOUTH);
-
-        frame.setSize(600, 400);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
 }
--- src/User.java
+++ src/User.java
@@ -1,23 +1,41 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.Socket;
 import java.util.ArrayList;
 
 public class User {
+    private PrintStream streamOut;
+    private InputStream streamIn;
+    private Socket client;
     public String name;
     public String username; //This will be used as the unique ID for each user
     private String password;
     public ArrayList<Conversation> conversations;  //all of the conversations this User participates in
 
-    public User(String name, String username, String password) {
+    public User(Socket client, String name, String username, String password) throws IOException {
+        this.streamOut = new PrintStream(client.getOutputStream());
+        this.streamIn = client.getInputStream();
+        this.client = client;
         this.name = name;
         this.username = username;
         this.password = password;
     }
 
-    public User(String name, String username, String password, ArrayList<Conversation> conversations) {
+    public User(Socket client, String name, String username, String password, ArrayList<Conversation> conversations) {
         this.name = name;
         this.username = username;
         this.password = password;
 
         //this.conversations = conversations;
+    }
+
+    public PrintStream getOutStream(){
+        return this.streamOut;
+    }
+
+    public InputStream getInputStream(){
+        return this.streamIn;
     }
 
     public String getName() {
